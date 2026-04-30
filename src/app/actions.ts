1fff68b84a87c1e88c78e7f02c262d023c60dc84
elcharitas <jonathanirhodia@gmail.com>
2026-04-30 15:21:12 +0100

    fix: add x-app-origin header to client requests

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -24,6 +24,7 @@ const client = createClient({
 			...options,
 			headers: {
 				...(options.headers as Record<string, string>),
+				"x-app-origin": "tourstack",
 				Cookie: cookieString,
 			},
 		};
